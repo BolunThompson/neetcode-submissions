c8ca7e098107ba777b801db850101bf3de3274c4
Bolun Thompson <abolunthompson@gmail.com>
2026-07-05 09:30:59 -0700

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.py
@@ -0,0 +1,28 @@
+def encode_str(s: str):
+    return f"{len(s)} {s}"
+
+def find_delim(s):
+    for i, v in enumerate(s):
+        if v == " ":
+            return i
+
+def decode_str(s):
+    delim = find_delim(s)
+    length = int(s[:delim])
+    start = delim + 1
+    end = delim + 1 + length
+    return s[start:end], s[end:]
+
+def decode_str_rec(s):
+    if not s:
+        return []
+    first, suffix = decode_str(s)
+    return [first] + decode_str_rec(suffix)
+
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        print("".join(encode_str(s) for s in strs))
+        return "".join(encode_str(s) for s in strs)
+
+    def decode(self, s: str) -> List[str]:
+        return decode_str_rec(s)
