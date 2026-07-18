3a3de7288ba8664628f5dd64fc22f8848986cab4
Bolun Thompson <abolunthompson@gmail.com>
2026-07-18 08:10:20 -0700

    Add: trapping-rain-water - submission-13

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-13.py
@@ -0,0 +1,26 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) <= 1:
+            return 0
+        s = 0
+        e = len(height) - 1
+        area = 0
+        max_height = min(height[s], height[e])
+        # TODO: Initial conds
+        while e > s:
+            old_s, old_e = height[s], height[e]
+            # advance smaller
+            if old_s < old_e:
+                while height[s] <= old_s and e > s:
+                    # we know that this > 0 since height[s] <= old_s <= max_height
+                    assert max_height - height[s] >= 0
+                    area += max_height - height[s]
+                    s += 1
+            else: # fine to advance if equal
+                while height[e] <= old_e and e > s:
+                    assert max_height - height[e] >= 0
+                    area += max_height - height[e]
+                    e -= 1
+            max_height = min(height[s], height[e])
+
+        return area
