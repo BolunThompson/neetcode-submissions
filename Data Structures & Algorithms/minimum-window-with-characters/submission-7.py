07c464c067403f36bdd6e1f190cbe39fa97e5d2e
Bolun Thompson <abolunthompson@gmail.com>
2026-07-22 17:53:56 -0700

    Add: minimum-window-with-characters - submission-7

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-7.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # if not present in t and at back shrink.
+        # if too many -> shrink.
+        cnts = Counter(t)
+        def is_stubble(c):
+            return cnts.get(c, -1) < 0
+
+        si = 0
+        stubble = 0
+        best_si, best_ei = 0, len(s) - 1
+        found = False
+        for ei, v in enumerate(s):
+            if v in cnts:
+                cnts[v] -= 1
+            if is_stubble(v):
+                stubble += 1
+            while is_stubble(s[si]) and si < ei:
+                if s[si] in cnts:
+                    cnts[s[si]] += 1
+                stubble -= 1
+                si += 1
+            window_size = ei - si + 1
+            cover = window_size - stubble
+            # cover - extra must be length of t, so it's t
+            if cover == len(t) and window_size <= (best_ei - best_si + 1):
+                best_si, best_ei = si, ei
+                found = True
+
+        
+        return s[best_si: best_ei + 1] if found else ""
+
+                
